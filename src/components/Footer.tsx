5a6755fb22b7ba24d5eef9ac549835b5b3da110e
Dominik Pezelj <pezelj.dominik@gmail.com>
2022-07-26 23:47:47 +0200

    Added Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,27 @@
+import { useTheme } from "@mui/material/styles";
+
 export const Footer = () => {
-  return <div>TEST</div>;
+  const { colors } = useTheme();
+  const FooterStyle = {
+    backgroundColor: colors.textMenuItems,
+    width: "100vw",
+    height: "5.375rem",
+    display: "flex",
+    bottom: "0",
+  };
+
+  const FooterTitle = {
+    color: colors.white,
+    fontFamily: "Roboto",
+    fontWeight: "400",
+    fontSize: "24px",
+    paddingLeft: "7.5rem",
+    alignItems: "center",
+    display: "flex",
+  };
+  return (
+    <div style={FooterStyle}>
+      <div style={FooterTitle}>Staycation</div>
+    </div>
+  );
 };
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,13 @@
 import { Navigation } from "./Navigation";
 import { Header } from "./Header";
+import { Footer } from "./Footer";
 
 export const Main = () => {
   return (
     <div>
       <Navigation />
       <Header />
+      <Footer />
     </div>
   );
 };
